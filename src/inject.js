b76634564ec677cb64559f433cbe1d5a629b62f5
Mesut Yiğit <mesut.yigit@fikrimuhal.com>
2017-12-01 12:41:33 +0300

    initial commit

---
+++ src/inject.js
@@ -0,0 +1,9 @@
+var sc1 = document.createElement("script");
+var sc2 = document.createElement("script");
+sc1.setAttribute("src", "https://static.hivecdn.com/hivecdn.js");
+sc2.setAttribute("src", "https://static.hivecdn.com/hivecdnjs-bitmovin-plugin.min.js");
+sc1.async = false;
+sc2.async = false;
+document.documentElement.appendChild(sc1);
+document.documentElement.appendChild(sc2);
+
---
+++ src/start.js
@@ -0,0 +1,42 @@
+function checkLoaded(variable, interval, maxCount) {
+    var count;
+    return new Promise(function (resolve, reject) {
+        var isLoaded = false;
+        var timerId = setInterval(function () {
+            count++;
+            isLoaded = window[variable] !== undefined;
+            if (count === maxCount || isLoaded) finish();
+        }, interval);
+        
+        function finish() {
+            console.log('EXT: -> ' + variable + 'loaded');
+            clearTimeout(timerId);
+            resolve(isLoaded);
+        }
+    });
+}
+
+checkLoaded('bitmovin', 100, 20).then(function (bitmovinLoaded) {
+    if (bitmovinLoaded) {
+        checkLoaded('hivecdn', 100, 20).then(function (hivecdnLoaded) {
+            if (hivecdnLoaded) {
+                //const player = bitmovin.player('playerLiveTvBitmovin');
+                if (document.getElementById('player') !== undefined) {
+                    const player = bitmovin.player('player');
+                    player.addEventHandler('onReady', function (event) {
+                        const url = player.getConfig().source.dash;
+                        hivecdn.registerPlayer(player, hivecdn.PlayerVendors.BITMOVIN, url, hivecdn.StreamTypes.DASH);
+                    });
+                }
+                
+            } else {
+                console.log('EXT: -> hivecdn not loaded');
+            }
+        });
+    } else {
+        console.log('EXT: -> bitmovin not loaded');
+    }
+});
+
+
+
