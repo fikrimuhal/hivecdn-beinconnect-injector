06220a7bf426b4af76358bab2cf2a23e3537f3d6
mesutyigit <yigitmesut92@gmail.com>
2017-12-12 16:11:17 +0300

    update script links

--- src/inject.js
+++ src/inject.js
@@ -15,8 +15,8 @@ function injectLibraries() {
     var sc1 = document.createElement("script");
     var sc2 = document.createElement("script");
 //var sc3 = document.createElement("script");
-    sc1.setAttribute("src", "https://static.hivecdn.com/hivecdn.min.js");
-    sc2.setAttribute("src", "https://static.hivecdn.com/hivecdnjs-bitmovin-plugin.min.js");
+    sc1.setAttribute("src", "https://static.hivecdn.com/hivecdn.min.js?v=2");
+    sc2.setAttribute("src", "https://static.hivecdn.com/hivecdnjs-bitmovin-plugin.min.js?v=2");
 //sc3.setAttribute("src", "https://static.hivecdn.com/hivecdnjs-demo-plugin.min.js");
     sc1.async = false;
     sc2.async = false;
