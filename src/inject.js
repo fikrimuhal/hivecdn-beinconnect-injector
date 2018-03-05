a9a2f82a7a395f56099ddff66551d5d0cb211db7
Mesut Yiğit <mesut.yigit@fikrimuhal.com>
2018-03-05 18:47:14 +0300

    demo plug aded

--- src/inject.js
+++ src/inject.js
@@ -1,9 +1,12 @@
 var debug = false;
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("document ready oldu");
     chrome.storage.sync.get(['inject'], function (conf) {
         if (conf !== undefined && (conf.inject === undefined || conf.inject === "true")) {
-            var scripts = ['https://static.hivecdn.com/hivecdn.production.min.js', 'https://static.hivecdn.com/hivecdnjs-bitmovin-plugin.production.min.js'];
+            var scripts = ['https://static.hivecdn.com/hivecdn.production.min.js',
+                'https://static.hivecdn.com/hivecdnjs-bitmovin-plugin.production.min.js',
+                'https://static.hivecdn.com/hivecdnjs-demo-plugin.production.min.js'
+            
+            ];
             var dev_scripts = ['http://mesut.ofis:9001/hivecdnv2client-fastopt-bundle.js', 'http://192.168.2.11:5010/hivecdnjs-bitmovin-plugin.min.js', 'http://mesut.ofis:5005/hivecdnjs-demo-plugin.min.js'];
             injectLibraries(debug ? dev_scripts : scripts);
         }
