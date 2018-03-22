9851ab007996b97acb36bd9f135826d95b03ef0e
Mesut Yiğit <mesut.yigit@fikrimuhal.com>
2018-03-22 14:03:27 +0300

    scripts fix

--- src/inject.js
+++ src/inject.js
@@ -1,26 +1,47 @@
 var debug = false;
+var scripts = {
+    production: [{
+        src: 'https://static.hivecdn.com/hivecdn.production.min.js', id: "hivecdnScript"
+    }, {
+        src: 'https://static.hivecdn.com/hivecdnjs-bitmovin-plugin.production.min.js'
+    }, {
+        src: 'https://static.hivecdn.com/hivecdnjs-demo-plugin.production.min.js'
+    }
+    
+    ], development: [{
+        src: 'http://192.168.2.11:9001/hivecdnv2client-fastopt-bundle.js', id: "hivecdnScript"
+    }, {
+        src: 'http://192.168.2.11:5010/hivecdnjs-bitmovin-plugin.min.js'
+    }, {
+        src: 'http://192.168.2.11:5005/hivecdnjs-demo-plugin.min.js'
+    }], stage: [{
+        src: 'https://static.hivecdn.com/hivecdn.master.min.js', id: "hivecdnScript"
+    }, {
+        src: 'https://static.hivecdn.com/hivecdnjs-bitmovin-plugin.master.min.js'
+    }, {
+        src: 'https://static.hivecdn.com/hivecdnjs-demo-plugin.master.min.js'
+    }]
+};
 document.addEventListener("DOMContentLoaded", function () {
     chrome.storage.sync.get(['inject'], function (conf) {
         if (conf !== undefined && (conf.inject === undefined || conf.inject === "true")) {
-            var scripts = ['https://static.hivecdn.com/hivecdn.production.min.js', 'https://static.hivecdn.com/hivecdnjs-bitmovin-plugin.production.min.js', 'https://static.hivecdn.com/hivecdnjs-demo-plugin.production.min.js'
-            
-            ];
-            var dev_scripts = ['http://mesut.ofis:9001/hivecdnv2client-fastopt-bundle.js', 'http://192.168.2.11:5010/hivecdnjs-bitmovin-plugin.min.js', 'http://mesut.ofis:5005/hivecdnjs-demo-plugin.min.js'];
-            var master_scripts = ['https://static.hivecdn.com/hivecdn.master.min.js', 'https://static.hivecdn.com/hivecdnjs-bitmovin-plugin.master.min.js', 'https://static.hivecdn.com/hivecdnjs-demo-plugin.master.min.js'];
-            injectLibraries(debug ? dev_scripts : scripts);
+            injectLibraries(debug ? scripts.development : scripts.production);
         }
     });
     
     
     function injectLibraries(links) {
-        links.forEach(function (src) {
-            inject(src);
+        links.forEach(function (script) {
+            inject(script);
         });
     }
     
-    function inject(src) {
+    function inject(script) {
+        var src = script.src;
+        var id = script.id;
         var _scriptElement = document.createElement("script");
         _scriptElement.setAttribute("src", src + "?v=" + Date.now());
+        if (id !== undefined) _scriptElement.setAttribute("id", id);
         _scriptElement.async = false;
         document.documentElement.appendChild(_scriptElement);
     }
